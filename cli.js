0467fe42690fc5516a006f7a7698dc5a483013f0
Brian C Sparks <briancsparks@gmail.com>
2023-03-24 19:49:20 -0700

    second

--- cli.js
+++ cli.js
@@ -2,3 +2,33 @@
 
 /* vim: set ft=javascript: */
 
+
+main().finally(() => {
+  // console.log('done');
+})
+
+async function main() {
+  const inquirer = await import('inquirer');
+  const prompt = inquirer.createPromptModule();
+
+  // inquirer.prompt([
+  //   {
+  //     type: 'list',
+  //     name: 'pizza',
+  //     message: 'What kind of pizza would you like?',
+  //     choices: ['pep', 'cheese', 'Hawaiian']
+  //   }
+  // ])
+
+
+  prompt([
+    {
+      type: 'list',
+      name: 'pizza',
+      message: 'What kind of pizza would you like?',
+      choices: ['pep', 'cheese', 'Hawaiian']
+    }
+  ]).then(answers => {
+    console.log(`You ordered a ${answers.pizza} pizza.`);
+  });
+}
